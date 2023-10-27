64cab878918d99ded2a1ef5a37ee06c357bb9286
Mohit <mohitk@shriffle.com>
2023-10-27 12:02:30 +0530

    add plan controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
-
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   resources :users
   resources :blogs
   resources :users do 
